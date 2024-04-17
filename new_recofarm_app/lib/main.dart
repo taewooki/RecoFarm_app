90e4ca12a031616ed0292e0abc4b1490fea7a7f0
pulpilisory <pulpilisory@gmailcom>
2024-04-17 15:45:36 +0900

    change commit

---
+++ new_recofarm_app/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:new_recofarm_app/firebase_options.dart';
+import 'view/home.dart';
+
+/*
+  * 
+  * Description : Main 
+  * Date        : 2024.04.17
+  * Author      : 
+  * Updates     : 
+  *   2024.04.17 by pdg
+  *     - main get x 사용할 수있도록 수정함 
+  *     - debug mode flag 없앰
+  *
+*/
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Reco-Farm Application',
+      localizationsDelegates: const [
+        // GlobalCupertinoLocalizations.delegate,
+        // GlobalMaterialLocalizations.delegate,
+        // GlobalWidgetsLocalizations.delegate
+      ],
+      supportedLocales: const [
+        Locale('ko', 'KR'),
+        Locale('en', 'US'),
+      ],
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Home(),
+    );
+  }
+}// END
