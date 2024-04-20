65f0db6bb830bafbde35a143b7b31147e4b9e58c
pulpilisory <pulpilisory@gmailcom>
2024-04-20 16:55:08 +0900

    pdg : splash effect

--- recofarm_integration_v01/lib/view/splash_screen.dart
+++ recofarm_integration_v01/lib/view/splash_screen.dart
@@ -26,69 +26,103 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   late Timer _timer;
-  late PageController pageController;
+  final PageController pageController = PageController();
 
   @override
   void initState() {
     super.initState();
-    pageController =PageController();
-    _timer= Timer.periodic(Duration(seconds: 2), (timer) {
-      Get.to(Home(),
-      transition: Transition.circularReveal
+    
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      //2 초에 한번씩 페이지가 넘어가는 스플래쉬 제작
+      int? nextpage = pageController.page?.toInt();
+      if (nextpage == null) {
+        return;
+      }
+      if (nextpage == 2) {
+        // splash 화면이 다되면 메인으로 넘어감.
+        Get.to(Home(), 
+        transition: Transition.fade,
+        curve:Curves.bounceOut ,
+        duration: Duration(seconds: 2),
+        );
+        _timer.cancel();
+      } else {
+        nextpage++;
+      }
+      pageController.animateToPage(
+        nextpage,
+        duration:  Duration(milliseconds: 700),
+        curve: Curves.bounceOut
+
       );
-      _timer.cancel();
-     });
+    });
   }
-  
+
   @override
   Widget build(BuildContext context) {
-    // system icon 들의 색상을 밝게 하여 이미지랑 어울리게 함. 
+    // system icon 들의 색상을 밝게 하여 이미지랑 어울리게 함.
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
-    return PageView(
-      controller: pageController,
-      children: [
-        Container(
-          
-          decoration: const BoxDecoration(
-            color: Color.fromARGB(255, 241, 178, 82),
-          ),
-          child:  Center(
-            child:  
-            Container(
-              width: 300,
-              height: 400,
-        
-              child:  Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset("images/farmer.png",
-                      width: 200,
-                      height: 200),
-                      //const LinearProgressIndicator(),
-                      const CircularProgressIndicator(
-                        valueColor: AlwaysStoppedAnimation(
-                          Color.fromARGB(255, 144, 213, 146)
-                        ),
-                      )
-                    ],
-                  ),
-                ],
+    return SafeArea(
+      top:true,
+      bottom: true,
+      child: PageView(
+        controller: pageController,
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              color: Color.fromARGB(255, 241, 178, 82),
+            ),
+            child: Center(
+              child: Container(
+                width: 300,
+                height: 400,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset("images/farmer.png", width: 200, height: 200),
+                        //const LinearProgressIndicator(),
+                        const CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation(
+                              Color.fromARGB(255, 144, 213, 146)),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
-        ),
-      //pageview images
-      Image.asset("images/splash_01.png",
-        fit:BoxFit.cover
-      ),
-       Image.asset("images/splash_02.png",
-        fit:BoxFit.cover
-      )
+          //pageview images
       
-      ],
+          Container(
+            decoration: const BoxDecoration(
+              color: Color.fromARGB(255, 241, 178, 82),
+            ),
+            child: Center(
+              child: Container(
+                width: 300,
+                height: 400,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset("images/splash_02.png", width: 300, height: 400),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          //Image.asset("images/splash_01.png", fit: BoxFit.cover),
+          Image.asset("images/splash_01.png", fit: BoxFit.contain)
+        ],
+      ),
     );
   }
 }
