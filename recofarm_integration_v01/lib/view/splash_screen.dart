390037f1c0322c988b2362db1ecadbaaf1d84c02
pulpilisory <pulpilisory@gmailcom>
2024-04-20 16:26:50 +0900

    pdg : splash작업

--- recofarm_integration_v01/lib/view/splash_screen.dart
+++ recofarm_integration_v01/lib/view/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:new_recofarm_app/view/login.dart';
 
@@ -12,6 +13,7 @@ import 'home.dart';
   * Updates     : 
   *   2024.04.20 by pdg
         - logo image plotting ( farmer logo )
+        - splash image 1, 2 삽입 
 
 */
 
@@ -24,53 +26,69 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   late Timer _timer;
+  late PageController pageController;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+    pageController =PageController();
     _timer= Timer.periodic(Duration(seconds: 2), (timer) {
-      Get.to(Home());
+      Get.to(Home(),
+      transition: Transition.circularReveal
+      );
       _timer.cancel();
      });
   }
   
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
+    // system icon 들의 색상을 밝게 하여 이미지랑 어울리게 함. 
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+    return PageView(
+      controller: pageController,
+      children: [
+        Container(
+          
+          decoration: const BoxDecoration(
+            color: Color.fromARGB(255, 241, 178, 82),
+          ),
+          child:  Center(
+            child:  
+            Container(
+              width: 300,
+              height: 400,
         
-        decoration: const BoxDecoration(
-          color: Color.fromARGB(255, 241, 178, 82),
-        ),
-        child:  Center(
-          child:  
-          Container(
-            width: 300,
-            height: 400,
-
-            child:  Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset("images/farmer.png",
-                    width: 200,
-                    height: 200),
-                    //const LinearProgressIndicator(),
-                    const CircularProgressIndicator(
-                      valueColor: AlwaysStoppedAnimation(
-                        Color.fromARGB(255, 144, 213, 146)
-                      ),
-                    )
-                  ],
-                ),
-              ],
+              child:  Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("images/farmer.png",
+                      width: 200,
+                      height: 200),
+                      //const LinearProgressIndicator(),
+                      const CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation(
+                          Color.fromARGB(255, 144, 213, 146)
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
+      //pageview images
+      Image.asset("images/splash_01.png",
+        fit:BoxFit.cover
       ),
+       Image.asset("images/splash_02.png",
+        fit:BoxFit.cover
+      )
+      
+      ],
     );
   }
 }
--- recofarm_integration_v01/lib/view/web_view_page.dart
+++ recofarm_integration_v01/lib/view/web_view_page.dart
@@ -9,6 +9,8 @@ import 'package:webview_flutter/webview_flutter.dart';
   Updates     : 
 	  2024.04.20 Sat by pdg
 		  - 별로 쓸때가 없지만 일단 플러터 복습하는김에 만들어 보았다.
+      - 농넷의 도매가 현황 사이트를 보여줌. 
+      - 뒤로가기 앞으로 가기 버튼 필요함? -> 일단 만듬. 
   Detail      : - 
 
 */
@@ -25,15 +27,12 @@ class _WebViewPageState extends State<WebViewPage> {
   late String siteName;
   late TextEditingController tfController;
 
-
   @override
   void initState() {
     super.initState();
     siteName = "www.nongnet.or.kr/";
-    isLoading =true;
-    tfController = TextEditingController(
-      text: siteName
-    );
+    isLoading = true;
+    tfController = TextEditingController(text: siteName);
     webViewController = WebViewController()
       ..setNavigationDelegate(
         NavigationDelegate(
@@ -54,10 +53,9 @@ class _WebViewPageState extends State<WebViewPage> {
             setState(() {});
           },
         ),
-      ) 
+      )
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..loadRequest(Uri.parse('https://$siteName'));
-     
   }
 
   @override
@@ -71,9 +69,55 @@ class _WebViewPageState extends State<WebViewPage> {
         centerTitle: true,
         backgroundColor: const Color.fromARGB(255, 255, 219, 109),
       ),
-      body: WebViewWidget(
-        controller: webViewController,
+      body: Stack(children: [
+        isLoading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : const Stack(),
+        WebViewWidget(
+          controller: webViewController,
+        ),
+      ]),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => backProcess(),
+        backgroundColor: Colors.amber,
+        child: Icon(Icons.arrow_back),
       ),
     );
   }
-}
+  // Function
+  backProcess() async{
+    if (await webViewController.canGoBack()){
+      webViewController.goBack();
+    }else{
+      snackbarFunction();
+    }
+  }
+      reloadProcess() async {
+    //Cpu 따로 쓰니까 어싱크, 다운로드 중에도 back 할수있으니까!
+    await webViewController.reload();
+  }
+  forwardProcess() async {
+    //Cpu 따로 쓰니까 어싱크, 다운로드 중에도 back 할수있으니까!
+    if (await webViewController.canGoForward()) {
+      // 뒤에 갈거있는지 물어본다.
+      webViewController.goForward();
+    } else {
+      snackbarFunction();
+    }
+  }
+  snackbarFunction() {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        backgroundColor: Theme.of(context).colorScheme.tertiary,
+        content: Text("뒤로 갈수 없습니다. "),
+        duration: Duration(seconds: 2),
+      ),
+    );
+  }
+    reloadSite() {
+    //siteName ="www.google.com";
+    webViewController.loadRequest(Uri.parse("https://$siteName"));
+  }
+}//End
